9ecd9b1a70f2dcce581e0e3c7f90a3fe5349efc4
gojimmypi <gojimmypi@gmail.com>
2019-04-27 11:22:42 -0700

    initial code

---
+++ GcodeHelper/GcodeHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GcodeHelperNamespace
+{
+    public class GcodeHelper
+    {
+        readonly string thisLine = "";
+
+        public List<GcodeItem> GcodeItems { get; }
+
+        public class GcodeItem
+        {
+            public string ItemText { get; }
+            public GcodeTokenTypes ItemType { get; }
+
+            public bool IsComment;
+
+            public GcodeItem(string itemtext, GcodeTokenTypes itemtype)
+            {
+                this.ItemText = itemtext;
+                this.ItemType = itemtype;
+                IsComment = false;
+            }
+
+        } //  public class GcodeItem
+
+        // init our GcodeHelper
+        public GcodeHelper(string item)
+        {
+            // item should be a single line of text, with no CR/LF
+            thisLine = item;
+            GcodeItems = new List<GcodeItem>();
+
+            for (int i = 0; i <= this.thisLine.Length - 1; i++)
+            {
+            
+                string thisChar = thisLine.Substring(i, 1);
+
+                // if a semicolon is encountered, the rest of the line is a comment
+                if (thisChar == ";")
+                {
+                    GcodeItems.Add(new GcodeItem(thisLine.Substring(i), GcodeTokenTypes.Comment));
+                }
+                string thisTargetTypeName;
+                switch (thisChar )
+                {
+                    case "-":
+                        thisTargetTypeName = "minus";
+                        break;
+                    default:
+                        thisTargetTypeName = thisChar;
+                        break;
+                }
+
+
+                // Get an enum item by string name (e.g. Gcode_A, Gcode_B, etc)
+                GcodeTokenTypes thisTokenType = GcodeTokenTypes.Undefined;
+                Enum.TryParse(thisTargetTypeName, out thisTokenType);
+                GcodeItems.Add(new GcodeItem(thisChar, thisTokenType));
+            }
+        }
+    }
+}
---
+++ GcodeTokenTypes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GcodeHelperNamespace
+{
+    public enum GcodeTokenTypes
+    {
+        Undefined,
+        Comment,
+        Gcode_A,
+        Gcode_B,
+        Gcode_C,
+        Gcode_D,
+        Gcode_E,
+        Gcode_F,
+        Gcode_G,
+        Gcode_H,
+        Gcode_I,
+        Gcode_J,
+        Gcode_K,
+        Gcode_L,
+        Gcode_M,
+        Gcode_N,
+        Gcode_O,
+        Gcode_P,
+        Gcode_Q,
+        Gcode_R,
+        Gcode_S,
+        Gcode_T,
+        Gcode_U,
+        Gcode_V,
+        Gcode_W,
+        Gcode_X,
+        Gcode_Y,
+        Gcode_Z,
+        Gcode_minus,
+        Gcode_0,
+        Gcode_1,
+        Gcode_2,
+        Gcode_3,
+        Gcode_4,
+        Gcode_5,
+        Gcode_6,
+        Gcode_7,
+        Gcode_8,
+        Gcode_9,
+        Gcode_10
+    }
+}
