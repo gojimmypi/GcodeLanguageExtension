f5fa8a4ca4ecc17fa29482ae46f0eb0a2b14d652
gojimmypi <gojimmypi@gmail.com>
2019-04-27 13:53:59 -0700

    improved comment detection

--- GcodeHelper/GcodeHelper.cs
+++ GcodeHelper/GcodeHelper.cs
@@ -32,7 +32,8 @@ namespace GcodeLanguage
             thisLine = item;
             GcodeItems = new List<GcodeItem>();
             bool FoundType = false;
-            GcodeTokenTypes ContinueType = GcodeTokenTypes.Undefined;
+            bool CommentActive = false; // true when whe are in a parenthesis-delimmited comment "(like this)"
+            GcodeTokenTypes thisBlockType = GcodeTokenTypes.Undefined;
             string thisBlock = "";
             for (int i = 0; i <= this.thisLine.Length - 1; i++)
             {
@@ -42,52 +43,94 @@ namespace GcodeLanguage
                 // if a semicolon is encountered, the rest of the line is a comment
                 if (thisChar == ";")
                 {
+                    if (thisBlock != "")
+                    {
+                        // if we have a prior block of text with a different highlight type, add it to the list before moving on
+                        GcodeItems.Add(new GcodeItem(thisBlock, thisBlockType));
+                        thisBlock = "";
+                    }
                     thisBlock = thisLine.Substring(i);
                     GcodeItems.Add(new GcodeItem(thisBlock, GcodeTokenTypes.Comment));
+                    thisBlock = "";
                     break;
                 }
 
-
+                // 
                 string thisTargetTypeName;
-                switch (thisChar)
+                if (thisChar == "(")
                 {
-                    case "-":
-                        thisTargetTypeName = "minus";
-                        break;
-                    default:
-                        thisTargetTypeName = "Gcode_" + thisChar;
-                        break;
-                }
-
-
-                // Get an enum item by string name (e.g. Gcode_A, Gcode_B, etc)
-                GcodeTokenTypes thisTokenType = GcodeTokenTypes.Undefined;
-                Enum.TryParse(thisTargetTypeName, out thisTokenType);
-                if (thisTokenType != GcodeTokenTypes.Undefined)
-                {
-                    if (!FoundType)
+                    if (thisBlock != "" && !CommentActive)
                     {
-                        ContinueType = thisTokenType;
-                        FoundType = true;
+                        // if we have a prior block of text with a different highlight type, add it to the list before moving on
+                        GcodeItems.Add(new GcodeItem(thisBlock, thisBlockType));
+                        thisBlock = "";
                     }
+                    CommentActive = true;
+                    thisBlock += thisChar;
+                }
+                else if (thisChar == ")" && CommentActive)
+                {
+                    thisBlock += thisChar;
+                    if (CommentActive)
+                    {
+                        GcodeItems.Add(new GcodeItem(thisBlock, GcodeTokenTypes.Comment));
+                        thisBlock = "";
+                    }
+                    CommentActive = false;
+                }
+                else if (CommentActive)
+                {
+                    thisBlock += thisChar;
                 }
                 else
                 {
-                    if (thisBlock != "")
+                    switch (thisChar)
                     {
-                        GcodeItems.Add(new GcodeItem(thisBlock, ContinueType));
-                        thisBlock = "";
+                        case "-":
+                            thisTargetTypeName = "minus";
+                            break;
+                        default:
+                            thisTargetTypeName = "Gcode_" + thisChar;
+                            break;
                     }
-                    ContinueType = GcodeTokenTypes.Undefined;
-                    FoundType = false;
+                    // Get an enum item by string name (e.g. Gcode_A, Gcode_B, etc)
+                    GcodeTokenTypes thisTokenType = GcodeTokenTypes.Undefined;
+                    Enum.TryParse(thisTargetTypeName, out thisTokenType);
+                    if (thisTokenType != GcodeTokenTypes.Undefined)
+                    {
+                        if (!FoundType)
+                        {
+                            if (thisBlock != "")
+                            {
+                                // if we have a prior block of text with a different highlight type, add it to the list before moving on
+                                GcodeItems.Add(new GcodeItem(thisBlock, thisBlockType));
+                                thisBlock = "";
+                            }
+                            thisBlockType = thisTokenType; // once a token is found, subsequence chars will share the type. (e.g. the g-code "G" token, followed by numbers)
+                            FoundType = true;
+                        }
+                    }
+                    else
+                    {
+                        if (thisBlock != "")
+                        {
+                            // if we have a prior block of text with a different highlight type, add it to the list before moving on
+                            GcodeItems.Add(new GcodeItem(thisBlock, thisBlockType));
+                            thisBlock = "";
+                        }
+                        thisBlockType = GcodeTokenTypes.Undefined;
+                        FoundType = false;
+                    }
+                    thisBlock += thisChar;
+
                 }
-                thisBlock += thisChar;
+
             }
 
             // if there's anthing left over, append it (e.g. a type that 
             if (thisBlock != "")
             {
-                GcodeItems.Add(new GcodeItem(thisBlock, ContinueType));
+                GcodeItems.Add(new GcodeItem(thisBlock, thisBlockType));
             }
         }
     }
