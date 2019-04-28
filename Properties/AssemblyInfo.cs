3e9c3b384c73a559516019258caa7d92eaafba50
gojimmypi <gojimmypi@gmail.com>
2019-04-28 11:34:39 -0700

    fussing with icon cropping

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GcodeLanguage")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Syntax Highlighting for Gcode in Visual Studio")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("gojimmypi")]
 [assembly: AssemblyProduct("GcodeLanguage")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("gojimmypi")]
+[assembly: AssemblyTrademark("gojimmypi")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.3")]
+[assembly: AssemblyFileVersion("0.1.1.3")]
 [assembly: Guid("63CEA96E-E88D-4966-ACAA-BF97CDDC31E4")]
 
