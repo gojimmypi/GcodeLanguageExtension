96e51d6074b184f480fe6d7251e0f57bfead15e0
gojimmypi <gojimmypi@gmail.com>
2019-04-27 23:26:58 -0700

    explicit type rather than substring(0,1)

--- GcodeTokenTag.cs
+++ GcodeTokenTag.cs
@@ -67,45 +67,45 @@ namespace GcodeLanguage
         {
             _buffer = buffer;
             _GcodeTypes = new Dictionary<string, GcodeTokenTypes>();
-            _GcodeTypes["A"] = GcodeTokenTypes.Gcode_A;
-            _GcodeTypes["B"] = GcodeTokenTypes.Gcode_B;
-            _GcodeTypes["C"] = GcodeTokenTypes.Gcode_C;
-            _GcodeTypes["D"] = GcodeTokenTypes.Gcode_D;
-            _GcodeTypes["E"] = GcodeTokenTypes.Gcode_E;
-            _GcodeTypes["F"] = GcodeTokenTypes.Gcode_F;
-            _GcodeTypes["G"] = GcodeTokenTypes.Gcode_G;
-            _GcodeTypes["H"] = GcodeTokenTypes.Gcode_H;
-            _GcodeTypes["I"] = GcodeTokenTypes.Gcode_I;
-            _GcodeTypes["J"] = GcodeTokenTypes.Gcode_J;
-            _GcodeTypes["K"] = GcodeTokenTypes.Gcode_K;
-            _GcodeTypes["L"] = GcodeTokenTypes.Gcode_L;
-            _GcodeTypes["M"] = GcodeTokenTypes.Gcode_M;
-            _GcodeTypes["N"] = GcodeTokenTypes.Gcode_N;
-            _GcodeTypes["O"] = GcodeTokenTypes.Gcode_O;
-            _GcodeTypes["P"] = GcodeTokenTypes.Gcode_P;
-            _GcodeTypes["Q"] = GcodeTokenTypes.Gcode_Q;
-            _GcodeTypes["R"] = GcodeTokenTypes.Gcode_R;
-            _GcodeTypes["S"] = GcodeTokenTypes.Gcode_S;
-            _GcodeTypes["T"] = GcodeTokenTypes.Gcode_T;
-            _GcodeTypes["U"] = GcodeTokenTypes.Gcode_U;
-            _GcodeTypes["V"] = GcodeTokenTypes.Gcode_V;
-            _GcodeTypes["W"] = GcodeTokenTypes.Gcode_W;
-            _GcodeTypes["X"] = GcodeTokenTypes.Gcode_X;
-            _GcodeTypes["Y"] = GcodeTokenTypes.Gcode_Y;
-            _GcodeTypes["Z"] = GcodeTokenTypes.Gcode_Z;
+            _GcodeTypes["Gcode_A"] = GcodeTokenTypes.Gcode_A;
+            _GcodeTypes["Gcode_B"] = GcodeTokenTypes.Gcode_B;
+            _GcodeTypes["Gcode_C"] = GcodeTokenTypes.Gcode_C;
+            _GcodeTypes["Gcode_D"] = GcodeTokenTypes.Gcode_D;
+            _GcodeTypes["Gcode_E"] = GcodeTokenTypes.Gcode_E;
+            _GcodeTypes["Gcode_F"] = GcodeTokenTypes.Gcode_F;
+            _GcodeTypes["Gcode_G"] = GcodeTokenTypes.Gcode_G;
+            _GcodeTypes["Gcode_H"] = GcodeTokenTypes.Gcode_H;
+            _GcodeTypes["Gcode_I"] = GcodeTokenTypes.Gcode_I;
+            _GcodeTypes["Gcode_J"] = GcodeTokenTypes.Gcode_J;
+            _GcodeTypes["Gcode_K"] = GcodeTokenTypes.Gcode_K;
+            _GcodeTypes["Gcode_L"] = GcodeTokenTypes.Gcode_L;
+            _GcodeTypes["Gcode_M"] = GcodeTokenTypes.Gcode_M;
+            _GcodeTypes["Gcode_N"] = GcodeTokenTypes.Gcode_N;
+            _GcodeTypes["Gcode_O"] = GcodeTokenTypes.Gcode_O;
+            _GcodeTypes["Gcode_P"] = GcodeTokenTypes.Gcode_P;
+            _GcodeTypes["Gcode_Q"] = GcodeTokenTypes.Gcode_Q;
+            _GcodeTypes["Gcode_R"] = GcodeTokenTypes.Gcode_R;
+            _GcodeTypes["Gcode_S"] = GcodeTokenTypes.Gcode_S;
+            _GcodeTypes["Gcode_T"] = GcodeTokenTypes.Gcode_T;
+            _GcodeTypes["Gcode_U"] = GcodeTokenTypes.Gcode_U;
+            _GcodeTypes["Gcode_V"] = GcodeTokenTypes.Gcode_V;
+            _GcodeTypes["Gcode_W"] = GcodeTokenTypes.Gcode_W;
+            _GcodeTypes["Gcode_X"] = GcodeTokenTypes.Gcode_X;
+            _GcodeTypes["Gcode_Y"] = GcodeTokenTypes.Gcode_Y;
+            _GcodeTypes["Gcode_Z"] = GcodeTokenTypes.Gcode_Z;
 
-            _GcodeTypes["minus"] = GcodeTokenTypes.Gcode_minus;
+            _GcodeTypes["Gcode_minus"] = GcodeTokenTypes.Gcode_minus;
 
-            _GcodeTypes["0"] = GcodeTokenTypes.Gcode_0;
-            _GcodeTypes["1"] = GcodeTokenTypes.Gcode_1;
-            _GcodeTypes["2"] = GcodeTokenTypes.Gcode_2;
-            _GcodeTypes["3"] = GcodeTokenTypes.Gcode_3;
-            _GcodeTypes["4"] = GcodeTokenTypes.Gcode_4;
-            _GcodeTypes["5"] = GcodeTokenTypes.Gcode_5;
-            _GcodeTypes["6"] = GcodeTokenTypes.Gcode_6;
-            _GcodeTypes["7"] = GcodeTokenTypes.Gcode_7;
-            _GcodeTypes["8"] = GcodeTokenTypes.Gcode_8;
-            _GcodeTypes["9"] = GcodeTokenTypes.Gcode_9;
+            _GcodeTypes["Gcode_0"] = GcodeTokenTypes.Gcode_0;
+            _GcodeTypes["Gcode_1"] = GcodeTokenTypes.Gcode_1;
+            _GcodeTypes["Gcode_2"] = GcodeTokenTypes.Gcode_2;
+            _GcodeTypes["Gcode_3"] = GcodeTokenTypes.Gcode_3;
+            _GcodeTypes["Gcode_4"] = GcodeTokenTypes.Gcode_4;
+            _GcodeTypes["Gcode_5"] = GcodeTokenTypes.Gcode_5;
+            _GcodeTypes["Gcode_6"] = GcodeTokenTypes.Gcode_6;
+            _GcodeTypes["Gcode_7"] = GcodeTokenTypes.Gcode_7;
+            _GcodeTypes["Gcode_8"] = GcodeTokenTypes.Gcode_8;
+            _GcodeTypes["Gcode_9"] = GcodeTokenTypes.Gcode_9;
 
             _GcodeTypes["comment"] = GcodeTokenTypes.comment;
 
@@ -153,11 +153,11 @@ namespace GcodeLanguage
                     // otherwise check to see if it is a keyword
                     else
                     {
-                        if (_GcodeTypes.ContainsKey(Item.ItemText.Substring(0, 1)))
+                        if (_GcodeTypes.ContainsKey(Item.ItemType.ToString()))
                         {
                             if (tokenSpan.IntersectsWith(curSpan))
                                 yield return new TagSpan<GcodeTokenTag>(tokenSpan,
-                                                                      new GcodeTokenTag(_GcodeTypes[Item.ItemText.Substring(0, 1)]));
+                                                                      new GcodeTokenTag(Item.ItemType));
                         }
                         else
                         {
